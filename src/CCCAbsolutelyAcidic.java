35774a12c69d46344d391a9702a0afdcd63203a7
beckett <91700492+beckettclarke@users.noreply.github.com>
2025-12-14 19:23:43 -0500

    Start CCC DMOJ

---
+++ src/CCCAbsolutelyAcidic.java
@@ -0,0 +1,14 @@
+// @problem  |  CCC 2010 J5 - Knight Hop
+// @author   |  Beckett Clarke
+// @version  |  3
+// @date     |  2025/11/24
+import java.util.Scanner;
+public class CCCAbsolutelyAcidic {
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    int readings = scan.nextInt();
+    for (int i = 0; i < readings; i++){
+      
+    }
+  }
+}
