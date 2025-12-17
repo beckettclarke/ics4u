ad0b264c2ac5c5e119421ac1fa635c72541d04fa
beckett <91700492+beckettclarke@users.noreply.github.com>
2025-12-16 20:21:14 -0500

    IT WORKS FINALLY

--- src/CCCAbsolutelyAcidic.java
+++ src/CCCAbsolutelyAcidic.java
@@ -1,58 +1,84 @@
 // @problem  |  CCC 2012 S3 - Absolutely Acidic
 // @author   |  Beckett Clarke
-// @version  |  1
+// @version  |  4
 // @date     |  2025-12-14
 import java.util.ArrayList;
 import java.util.Scanner;
 public class CCCAbsolutelyAcidic {
+
+  static  ArrayList<Integer> result = new ArrayList<>();
+  static  ArrayList<Integer> result2 = new ArrayList<>();
   public static void main(String[] args){
     Scanner scan = new Scanner(System.in);
+    // Gets all the inputs (# of readings and the readings themselves)
     int readcount = scan.nextInt();
     ArrayList<Integer> readings = new ArrayList<>();
     ArrayList<Integer> frequencies = new ArrayList<>();
     for (int i = 0; i < readcount; i++){
       int r = scan.nextInt();
       int ri = readings.indexOf(r);
+      // Stores the readings by increasing the frequency if it already exists rather than making a new duplicate entry to make things easier later
       if (ri != -1){
         frequencies.set(ri,frequencies.get(ri)+1);
       } else {
-        readings.add(r);
+        readings.add(r):
         frequencies.add(1);
       }
     }
     // DONE GETTING INPUT, START PROCESSING
 
-    // Get the max and min values
-    ArrayList<Integer> max = findminmax(frequencies, 1);
-    ArrayList<Integer> min = findminmax(frequencies, -1);
+    // Get the max and second max values
+    findminmax(frequencies);
     int maxdif = 0;
-    // Go through every possible combination of mins and maxes, see which has the biggest difference between the two
-    for (int i = 0; i < max.size(); i++){
-      for (int k = 0; k < min.size(); k++){
-        int diff = Math.abs(readings.get(max.get(i)) - readings.get(min.get(k)));
-        if (diff > maxdif){
-          maxdif = diff;
+    
+    // IF THE MOST FREQUENT LIST HAS MULTIPLE VALUES, Only compare those values
+    if (result.size() > 1) {
+      // Multiple readings have the max frequency - find max difference among them
+      for (int i = 0; i < result.size(); i++){ 
+        for (int k = 0; k < result.size(); k++){
+          int diff = Math.abs(readings.get(result.get(i)) - readings.get(result.get(k))); // Find difference between the two
+          if (diff > maxdif){ // Update maxdif if this is the largest difference found
+            maxdif = diff;
+          }
+        }
+      }
+    } else {
+      // IF IT DOESNT, compare most frequent with second most frequent
+      // Loops over all possible combos of the two
+      for (int i = 0; i < result.size(); i++){ 
+        for (int k = 0; k < result2.size(); k++){
+          int diff = Math.abs(readings.get(result.get(i)) - readings.get(result2.get(k)));
+          if (diff > maxdif){ // Update if this is the largest difference found
+            maxdif = diff;
+          }
         }
       }
     }
     System.out.println(maxdif);
   }
 
-  // This basically finds all candidates that are at the min value or the max value
-  public static ArrayList<Integer> findminmax(ArrayList<Integer> search, int mode){
-    // searching mode: max = 1, min = -1, saves having to have 2 separate methods to do very simliar things
-    ArrayList<Integer> result = new ArrayList<>(); // Arraylist stores all values that occur the min/max
-    int minmax = (mode == 1) ? -1 : Integer.MAX_VALUE; // Current maximum or minimum frequency
+  // This basically finds all candidates that are at the max value and second max value
+  public static void findminmax(ArrayList<Integer> search){
+    int minmax = -1; // Current maximum frequency
+    int minmax2 = 0; // Current second maximum frequency
     for (int i = 0; i < search.size(); i++){ // For every item in the frequency list
       int cur = search.get(i);
-      if ((mode == 1 && cur > minmax) || (mode == -1 && cur < minmax)){ // If the current frequency is a min/max based on mode
-        result.clear(); // Remove all old items since they arent useful anymore (New min/max found)
+      if (cur > minmax){ // If the current frequency is a max
+        result2.clear(); // Move old max to second max
+        result2.addAll(result);
+        minmax2 = minmax;
+        result.clear(); // Remove all old items since they arent useful anymore (New max found)
         result.add(i);
         minmax = cur;
       } else if (minmax == cur) {
-        result.add(i); // Add the current one as candidate since it is the same frequency as the minmax
+        result.add(i); // Add the current one as candidate since it is the same frequency as the max
+      } else if (cur > minmax2) {
+        result2.clear(); // New second max found
+        result2.add(i);
+        minmax2 = cur;
+      } else if (minmax2 == cur) {
+        result2.add(i); // Add the current one as candidate since it is the same frequency as the second max
       }
     }
-    return result;
   }
 }
