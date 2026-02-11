56bb28afdff6f6a35f409de28f9272dfaf4a909d
beckett <91700492+beckettclarke@users.noreply.github.com>
2026-02-11 15:17:16 -0500

    Alpha channel for EVERYTHING and corner rounding support

--- src/picturemagick.java
+++ src/picturemagick.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import javax.imageio.ImageIO;
 
 public class picturemagick {
-  static int[][] red, green, blue, ored, ogreen, oblue;
+  static int[][] red, green, blue, alpha, ored, ogreen, oblue, oalpha;
   static int width, height;
   
   public static void main(String[] args) throws IOException{
@@ -36,15 +36,18 @@ public class picturemagick {
     red = new int[height][width];
     green = new int[height][width];
     blue = new int[height][width];
+    alpha = new int[height][width];
     // These will store the values that have been processed later on
     ored = new int[height][width];
     ogreen = new int[height][width];
-    oblue = new int[height][width]; 
+    oblue = new int[height][width];
+    oalpha = new int[height][width];
 
     // Loops over each pixel, adds red green and blue values to array 
     for (int y = 0; y < height; y++) {
       for (int x = 0; x < width; x++) {
         int pixel = img.getRGB(x, y);
+        alpha[y][x] = (pixel >> 24) & 0xFF;
         red[y][x] = (pixel >> 16) & 0xFF;
         green[y][x] = (pixel >> 8) & 0xFF;
         blue[y][x] = pixel & 0xFF;
@@ -59,6 +62,7 @@ public class picturemagick {
       System.out.println("2 | Box Blur");
       System.out.println("3 | Overexpose");
       System.out.println("4 | Greyscale");
+      System.out.println("5 | Corner Round");
       System.out.println("0 | Quit");
       System.out.print("Action to apply: ");
       choice = scan.nextInt();
@@ -68,26 +72,33 @@ public class picturemagick {
           int gbr = scan.nextInt();
           GaussianBlur(gbr);
           pushNewImg();
-          saveimage(path.substring(0, path.indexOf("."))+ "-modified.png", ored, ogreen, oblue);
+          saveimage(path.substring(0, path.indexOf(".")) + "-modified.png", ored, ogreen, oblue, oalpha);
           break;
         case 2:
           System.out.print("Enter blur radius: ");
           int bbr = scan.nextInt();
           BoxBlur(bbr);
           pushNewImg();
-          saveimage(path.substring(0, path.indexOf("."))+ "-modified.png", ored, ogreen, oblue);
+          saveimage(path.substring(0, path.indexOf(".")) + "-modified.png", ored, ogreen, oblue, oalpha);
           break;
         case 3:
           System.out.print("Enter overexpose amount: ");
           int over = scan.nextInt();
           Overexpose(over);
           pushNewImg();
-          saveimage(path.substring(0, path.indexOf("."))+ "-modified.png", ored, ogreen, oblue);
+          saveimage(path.substring(0, path.indexOf(".")) + "-modified.png", ored, ogreen, oblue, oalpha);
           break;
         case 4:
           Greyscale();
           pushNewImg();
-          saveimage(path.substring(0, path.indexOf("."))+ "-modified.png", ored, ogreen, oblue);
+          saveimage(path.substring(0, path.indexOf(".")) + "-modified.png", ored, ogreen, oblue, oalpha);
+          break;
+        case 5:
+          System.out.print("Enter corner rounding radius: ");
+          int r = scan.nextInt();
+          CornerRound(r);
+          pushNewImg();
+          saveimage(path.substring(0, path.indexOf(".")) + "-modified.png", ored, ogreen, oblue, oalpha);
           break;
         case 0:
           System.out.println("Exiting...");
@@ -108,6 +119,7 @@ public class picturemagick {
         red[y][x] = ored[y][x];
         green[y][x] = ogreen[y][x];
         blue[y][x] = oblue[y][x];
+        alpha[y][x] = oalpha[y][x];
       }
     }
   }
@@ -123,7 +135,7 @@ public class picturemagick {
     for (int i = 0; i < width; i++){
       System.out.print("\rProcessing row [" + (i+1) + "/" + width + "]");
       for (int k = 0; k < height; k++){
-        double rtotal = 0, gtotal = 0, btotal = 0;
+        double rtotal = 0, gtotal = 0, btotal = 0, atotal = 0;
         // For each neighbouring pixel within the radius
         for (int j = -radius; j <= radius; j++){
           for (int l = -radius; l <= radius; l++){
@@ -135,6 +147,7 @@ public class picturemagick {
               rtotal += red[lookingaty][lookingatx] * weight;
               gtotal += green[lookingaty][lookingatx] * weight;
               btotal += blue[lookingaty][lookingatx] * weight;
+              atotal += alpha[lookingaty][lookingatx] * weight;
             }
           }
         }
@@ -142,6 +155,7 @@ public class picturemagick {
         ored[k][i] = (int) Math.min(Math.max(Math.round(rtotal), 0), 255);
         ogreen[k][i] = (int) Math.min(Math.max(Math.round(gtotal), 0), 255);
         oblue[k][i] = (int) Math.min(Math.max(Math.round(btotal), 0), 255);
+        oalpha[k][i] = (int) Math.min(Math.max(Math.round(atotal), 0), 255);
         
       }
     }
@@ -209,6 +223,7 @@ public class picturemagick {
         ored[k][i] = (int) Math.min(Math.max(rtotal / 20, 0), 255);
         ogreen[k][i] = (int) Math.min(Math.max(gtotal / 20, 0), 255);
         oblue[k][i] = (int) Math.min(Math.max(btotal / 20, 0), 255);
+        oalpha[k][i] = alpha[k][i];
         
       }
     }
@@ -243,7 +258,7 @@ public class picturemagick {
       System.out.print("\rProcessing row [" + (i+1) + "/" + width + "]");
       for (int k = 0; k < height; k++){
         // Variables to store the total of each value
-        int rtotal = 0, gtotal = 0, btotal = 0;
+        int rtotal = 0, gtotal = 0, btotal = 0, atotal = 0;
         int count = 0; // Stores how many values have been added 
         // For each neighbouyring pixel based on radius
         for (int j = -radius; j <= radius; j++){
@@ -252,6 +267,7 @@ public class picturemagick {
             rtotal += red[k][lookingat];
             gtotal += green[k][lookingat];
             btotal += blue[k][lookingat];
+            atotal += alpha[k][lookingat];
             count++;
           }
         }
@@ -260,6 +276,7 @@ public class picturemagick {
         ored[k][i] = rtotal / count;
         ogreen[k][i] = gtotal / count;
         oblue[k][i] = btotal / count;
+        oalpha[k][i] = atotal / count;
       }
     }
     // For each pixel in each column
@@ -267,7 +284,7 @@ public class picturemagick {
       System.out.print("\rProcessing column [" + (i+1) + "/" + height + "]");
       for (int k = 0; k < width; k++){
         // Variables to store the total of each value
-        int rtotal = 0, gtotal = 0, btotal = 0;
+        int rtotal = 0, gtotal = 0, btotal = 0, atotal = 0;
         int count = 0; // Stores how many values have been added
         // For each neighbouyring pixel based on radius
         for (int j = -radius; j <= radius; j++){
@@ -276,6 +293,7 @@ public class picturemagick {
             rtotal += ored[lookingat][k];
             gtotal += ogreen[lookingat][k];
             btotal += oblue[lookingat][k];
+            atotal += oalpha[lookingat][k];
             count++;
           }
         }
@@ -283,6 +301,7 @@ public class picturemagick {
         ored[i][k] = rtotal / count;
         ogreen[i][k] = gtotal / count;
         oblue[i][k] = btotal / count;
+        oalpha[i][k] = atotal / count;
       }
     }
     System.out.println();
@@ -303,26 +322,79 @@ public class picturemagick {
         ored[y][x] = grey;
         ogreen[y][x] = grey;
         oblue[y][x] = grey;
+        oalpha[y][x] = alpha[y][x];
       }
     }
   }
 
   // =================================================================================================================================== //
 
-  public static void saveimage(String output, int[][] red, int[][] green, int[][] blue) throws IOException {
+  public static void CornerRound(int r){
+    // Clone original image
+    for (int y = 0; y < height; y++) {
+      for (int x = 0; x < width; x++) {
+        ored[y][x] = red[y][x];
+        ogreen[y][x] = green[y][x];
+        oblue[y][x] = blue[y][x];
+        oalpha[y][x] = alpha[y][x];
+      }
+    }
+
+    // For each corner
+    for (int n = 1; n <= r && n <= height && n <= width; n++) {
+      // How many pixels to remove from this row
+      int removepixels = (int) Math.round(r - Math.sqrt(r * r - Math.pow(r - n + 1, 2)));
+      
+      // Top left
+      for (int x = 0; x < removepixels && x < width; x++) {
+        ored[n - 1][x] = 0;
+        ogreen[n - 1][x] = 0;
+        oblue[n - 1][x] = 0;
+        oalpha[n - 1][x] = 0;
+      }
+      
+      // Top right
+      for (int x = width - removepixels; x < width && x >= 0; x++) {
+        ored[n - 1][x] = 0;
+        ogreen[n - 1][x] = 0;
+        oblue[n - 1][x] = 0;
+        oalpha[n - 1][x] = 0;
+      }
+      
+      // Bottom left
+      for (int x = 0; x < removepixels && x < width; x++) {
+        ored[height - n][x] = 0;
+        ogreen[height - n][x] = 0;
+        oblue[height - n][x] = 0;
+        oalpha[height - n][x] = 0;
+      }
+      
+      // Bottom right
+      for (int x = width - removepixels; x < width && x >= 0; x++) {
+        ored[height - n][x] = 0;
+        ogreen[height - n][x] = 0;
+        oblue[height - n][x] = 0;
+        oalpha[height - n][x] = 0;
+      }
+    }
+    System.out.println("Corner rounded with radius: " + r);
+  }
+
+  // =================================================================================================================================== //
+
+  public static void saveimage(String output, int[][] red, int[][] green, int[][] blue, int[][] alpha) throws IOException {
     // Creates empty image for everything to go into
-    BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
     // For every pixel in the image
     for (int y = 0; y < height; y++) {
       for (int x = 0; x < width; x++) {
         // Put the red green and blue values into one pixel memory adress integer and set the corresponding pixel
-        int rgb = (red[y][x] << 16) | (green[y][x] << 8) | blue[y][x];
-        img.setRGB(x, y, rgb);
+        int argb = (alpha[y][x] << 24) | (red[y][x] << 16) | (green[y][x] << 8) | blue[y][x];
+        img.setRGB(x, y, argb);
       }
     }
     // Saves the file to the same directory as originally inputted image
     ImageIO.write(img, "png", new File(output));
     System.out.println("Image exported to " + output);
-
   }
 }
